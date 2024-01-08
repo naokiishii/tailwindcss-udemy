a1ff687011ff986453933a2384921704052b9110
Naoki <ishii.naoki@ymail.ne.jp>
2024-01-08 11:12:56 +0900

    first commit(navigation)

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          100: "#ebf8ff",
+          300: "#90cdf4",
+          500: "#4299e1",
+        },
+        body: "#050505",
+        "selected-text": "#a3a3ff",
+      "theme": "#5c318c",
+      }
+    },
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"]
+    }
+  },
+  plugins: [],
+}
+
