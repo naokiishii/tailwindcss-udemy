63075979f7486c78632062932d901aa52c4b7fef
Naoki <ishii.naoki@ymail.ne.jp>
2024-01-10 09:04:06 +0900

    add skills section

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,9 @@ module.exports = {
         },
         body: "#050505",
         "selected-text": "#a3a3ff",
-      "theme": "#5c318c",
+      theme: "#5c318c",
+      secondary: "#9191a4",
+      badge: "#3f3f51",
       }
     },
     fontFamily: {
